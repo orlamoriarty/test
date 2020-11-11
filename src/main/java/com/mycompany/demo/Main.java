4fe228d5fdf831d0037fb82074f72a138f28c5be
Moriarty <Moriarty@DESKTOP-5H0S27A>
2020-11-11 15:35:22 +0000

    changed hello message

--- src/main/java/com/mycompany/demo/Main.java
+++ src/main/java/com/mycompany/demo/Main.java
@@ -12,7 +12,7 @@ package com.mycompany.demo;
 public class Main {
     
     public static void main (String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello IS3313");
     }
     
 }
